771b241b0be2a77d445d71a8a55966b7ad67e4ff
Ilnur <ilnur.gaet@gmail.com>
2021-07-14 23:25:16 +0600

    hw2

--- app/Controllers/SiteController.php
+++ app/Controllers/SiteController.php
@@ -6,6 +6,7 @@ namespace App\Controllers;
 
 use App\Models\Book;
 use Laminas\Diactoros\Response\TextResponse;
+use League\Route\Http\Exception\NotFoundException;
 
 class SiteController
 {
@@ -19,13 +20,14 @@ class SiteController
     function create(){
         return view('create.twig');
     }
+
     function store(){
         $name=request('name');
         $book=new Book();
         $book->setName($name);
         $book->save();
 
-        return redirect("/");
+        return redirect("/list");
     }
     function home(){
         $name=$_SESSION['name']??null;
@@ -37,5 +39,40 @@ class SiteController
         $name=request('name');
         $_SESSION['name']=$name;
         return redirect("/");
+    }
+    public function show($req, $args)
+    {
+        $book=((Book::query()->findBy(["id"=> $args["id"]]))[0]);
+        if($book!=null){
+            return view('show.twig', [
+                'book'=>$book
+            ]);
+        }
+        throw new NotFoundException();
+    }
+    public function delete()
+    {
+        $id=request("id");
+        $book=((Book::query()->findBy(["id"=> $id]))[0]);
+        $book->remove();
+        return redirect("/list");
+    }
+    function edit($req, $args){
+        $book=((Book::query()->findBy(["id"=> $args["id"]]))[0]);
+        if($book!=null){
+            return view('create.twig',[
+                'book'=>$book
+            ]);
+        }
+        throw new NotFoundException();
+    }
+    function update(){
+        $id=request('id');
+        $name=request("name");
+        $book=((Book::query()->findBy(["id"=> $id]))[0]);
+        $book->setName($name);
+        $book->save();
+
+        return redirect("/list/{$id}");
     }
 }
